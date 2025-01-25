2358d7d81c4ca07ad0748dbcef1b07b95ca16e28
oUesio <williamchengluong@gmail.com>
2025-01-25 19:47:31 +0000

    Add pyshoe edited

---
+++ ins_tools/EKF.py
@@ -0,0 +1,159 @@
+import numpy as np
+from numpy import linalg as LA
+from ins_tools.util import *
+from ins_tools.geometry_helpers import quat2mat, mat2quat, euler2quat, quat2euler
+import sys
+sys.path.append('../')
+
+class Localizer():
+    def __init__(self, config, imudata):
+        # config: A dictionary of configuration parameters
+        self.config = config
+        self.imudata = imudata
+    def init(self):
+        imudata = self.imudata 
+        # Initialise state to be at 0
+        x = np.zeros((imudata.shape[0],9))
+        # #Initialise quaternion 
+        q = np.zeros((imudata.shape[0],4))
+        avg_x = np.mean(imudata[0:20,0])
+        avg_y = np.mean(imudata[0:20,1])
+        avg_z = np.mean(imudata[0:20,2]) #gets avg accelerometer values for finding roll/pitch
+        
+        heading = 0
+        roll = np.arctan2(-avg_y,-avg_z)
+        pitch = np.arctan2(avg_x,np.sqrt(avg_y*avg_y + avg_z*avg_z))
+           
+        attitude = np.array([roll, pitch, heading])
+        x[0, 6:9] = attitude
+        q[0, :] = euler2quat(roll, pitch, heading, 'sxyz')
+
+        # Initialise covariance matrix P
+        P_hat = np.zeros((imudata.shape[0],9,9))
+        P_hat[0,0:3,0:3] = np.power(1e-5,2)*np.identity(3) #position (x,y,z) variance
+        P_hat[0,3:6,3:6] = np.power(1e-5,2)*np.identity(3) #velocity (x,y,z) variance
+        P_hat[0,6:9,6:9] = np.power(0.1*np.pi/180,2)*np.identity(3) #np.power(0.1*np.pi/180,2)*np.identity(3)
+        return x, q, P_hat
+    
+    # Updates the state vector based on the motion model
+    def nav_eq(self,xin,imu,qin,dt):
+        #update Quaternions
+        x_out = np.copy(xin) #initialize the output
+        omega = np.array([[0,-imu[3], -imu[4], -imu[5]],  [imu[3], 0, imu[5], -imu[4]],  [imu[4], -imu[5], 0, imu[3]],  [imu[5], imu[4], -imu[3], 0]])
+
+        # Use gyroscope data to update the quaternion, apply angular velocity using Rodrigues’ rotation formula.
+        norm_w = LA.norm(imu[3:6])
+        if(norm_w*dt != 0):
+            q_out = (np.cos(dt*norm_w/2)*np.identity(4) + (1/(norm_w))*np.sin(dt*norm_w/2)*omega).dot(qin) 
+        else:
+            q_out = qin
+
+        attitude = quat2euler(q_out,'sxyz')#update euler angles
+        x_out[6:9] = attitude    
+        
+        Rot_out = quat2mat(q_out)   #get rotation matrix from quat
+        acc_n = Rot_out.dot(imu[0:3])       #transform acc to navigation frame,  
+        acc_n = acc_n + np.array([0,0,self.config["g"]])   #removing gravity (by adding)
+        
+        #Update velocity  and position using kinematic equations
+        x_out[3:6] += dt*acc_n #velocity update
+        x_out[0:3] += dt*x_out[3:6] +0.5*np.power(dt,2)*acc_n #position update
+        
+        # Updated state vector, Updated quaternion, Rotation matrix .
+        return x_out, q_out, Rot_out    
+    
+    # Computes the state transition (F) and noise Jacobian (G) matrices for the EKF
+    def state_update(self, imu,q, dt):
+        # State Transition Matrix, Models the relationship between states (position, velocity, orientation) over time
+        # Accounts for linear velocity update and the effect of rotation on acceleration
+        F = np.identity(9)
+        F[0:3,3:6] = dt*np.identity(3)
+
+        Rot = quat2mat(q)
+        imu_r = Rot.dot(imu[0:3])
+        f_skew = np.array([[0,-imu_r[2],imu_r[1]],[imu_r[2],0,-imu_r[0]],[-imu_r[1],imu_r[0],0]])
+        F[3:6,6:9] = -dt*f_skew 
+        
+        # Noise Jacobian Matrix, Maps process noise (accelerometer and gyroscope noise) into the state space
+        # Includes contributions of rotation matrix
+        G = np.zeros((9,6))
+        G[3:6,0:3] = dt*Rot
+        G[6:9,3:6] = -dt*Rot
+       
+        return F,G
+    
+    # Corrects the predicted state using zero-velocity updates (ZUPTs) and the Kalman filter correction step
+    def corrector(self, x_check, P_check, Rot):
+        eye3 = np.identity(3)
+        eye9 = np.identity(9)
+        omega = np.zeros((3,3))        
+        
+        # Uses the measurement model (H matrix) and measurement noise (R) to compute the Kalman gain
+        K = (P_check.dot(self.config["H"].T)).dot(LA.inv((self.config["H"].dot(P_check)).dot(self.config["H"].T) + self.config["R"]))
+        z = -x_check[3:6] ### true state is 0 velocity, current velocity is error
+        q=mat2quat(Rot)   
+        dx = K.dot(z) 
+        x_check += dx  ###inject position and velocity error
+         
+        omega[0:3,0:3] = [[0,-dx[8], dx[7]],[dx[8],0,-dx[6]],[-dx[7],dx[6],0]] 
+        Rot = (eye3+omega).dot(Rot)
+        q = mat2quat(Rot)
+        attitude = quat2euler(q,'sxyz')
+        x_check[6:9] = attitude    #Inject rotational error 
+        # Apply the Kalman filter correction step to update the covariance matrix          
+        P_check = (eye9-K.dot(self.config["H"])).dot(P_check)
+        P_check = (P_check + P_check.T)/2
+        # Corrected state vector (x_check), covariance matrix (P_check), and quaternion (q)
+        return x_check, P_check, q
+
+    # Stationary Hypothesis Optimal Estimator (SHOE)
+    def SHOE(self, W=5):
+        imudata = self.imudata
+        # Divide the IMU data into windows, Compute test statistics for each window
+        T = np.zeros(np.int(np.floor(imudata.shape[0]/W)+1))
+        zupt = np.zeros(imudata.shape[0])
+        a = np.zeros((1,3))
+        w = np.zeros((1,3))
+        # Compute inverse of noise variances
+        inv_a = (1/self.config["var_a"])
+        inv_w = (1/self.config["var_w"])
+        # First 3 is accelerometer data
+        acc = imudata[:,0:3]
+        # Next 3 is angular velocity
+        gyro = imudata[:,3:6]
+    
+        i=0
+        print ('6. Loop through IMU data in windows of size W')
+        print ('7. Windows: %s - %s (%s)' % (0, imudata.shape[0]-W+1, W))
+        for k in range(0,imudata.shape[0]-W+1,W): #filter through all imu readings
+            # Compute mean acceleration for window
+            smean_a = np.mean(acc[k:k+W,:],axis=0)
+            # Loop through each time step in the window
+            for s in range(k,k+W):
+                # Update acceleration and angular velocity
+                a.put([0,1,2],acc[s,:])
+                w.put([0,1,2],gyro[s,:])
+                # Add contribution of acceleration to test statistic
+                T[i] += inv_a*( (a - self.config["g"]*smean_a/LA.norm(smean_a)).dot(( a - self.config["g"]*smean_a/LA.norm(smean_a)).T)) #acc terms
+                # Add contribution of angular velocity to the test statistic
+                T[i] += inv_w*( (w).dot(w.T) )
+            zupt[k:k+W].fill(T[i])
+            i+=1
+        zupt = zupt/W
+        # Zero-velocity likelihood
+        print ('8. Output zupt likelihood')
+        return zupt
+        
+
+    ### if a custom zero-velocity detector was added as a function above, additionally modify this list:    
+    def compute_zv_lrt(self, W=5, G=3e8, detector='shoe', return_zv=True): #import window size, zv-threshold
+        # Compares likelihoods  against a threshold to determine whether zero velocity is detected
+        print ('5. Choose shoe detector (compute_zv_lrt)')
+        if detector=='shoe':    
+            zv = self.SHOE(W=W)
+        print ('9. More stuff for zupt')
+        if return_zv:
+            zv=zv<G
+        return zv
+
+        
---
+++ ins_tools/INS.py
@@ -0,0 +1,100 @@
+import numpy as np
+from ins_tools.util import *
+from ins_tools.EKF import Localizer
+
+class INS():
+    def __init__(self, imudata, sigma_a=0.01, sigma_w=0.1*np.pi/180, T=1.0/125, dt=None):
+        self.config = {
+        "sigma_a": sigma_a,
+        "sigma_w": sigma_w,
+        "g": 9.8029,
+        "T": T,
+        "dt": dt,
+            }
+        # Stores the input IMU data
+        self.imudata = imudata
+        # Noise standard deviations
+        self.sigma_a = self.config["sigma_a"]
+        self.sigma_w = self.config["sigma_w"]
+        self.var_a = np.power(self.sigma_a,2)
+        self.config["var_a"] = self.var_a
+        self.var_w = np.power(self.sigma_w,2)
+        self.config["var_w"] = self.var_w
+
+        self.g = self.config["g"]
+        self.T = self.config["T"]
+        ##process noise in body frame
+        # Variances of the accelerometer and gyroscope noise
+        print ('2. Calc variances and standard deviations')
+        self.sigma_acc = 0.5*np.ones((1,3))
+        self.var_acc = np.power(self.sigma_acc,2)
+        self.sigma_gyro = 0.5*np.ones((1,3))*np.pi/180
+        self.var_gyro = np.power(self.sigma_gyro,2)
+        ##process noise covariance matrix Q
+        # Combines the variances of accelerometer and gyroscope noises into a covariance matrix
+        print ('3. Covariance matrix')
+        self.Q = np.zeros((6,6)) 
+        self.Q[0:3,0:3] = self.var_acc*np.identity(3)
+        self.Q[3:6,3:6] = self.var_gyro*np.identity(3)
+        self.config["Q"] = self.Q
+        # Measurement noise covariance matrix for velocity measurements
+        # Assumes a small noise level (0.01)
+        self.sigma_vel = 0.01 #0.01 default
+        self.R = np.zeros((3,3))
+        self.R[0:3,0:3] = np.power(self.sigma_vel,2)*np.identity(3)   ##measurement noise, 0.01 default
+        self.config["R"] = self.R
+        # Measurement matrix mapping state to velocity measurements
+        self.H = np.zeros((3,9))
+        self.H[0:3,3:6] = np.identity(3)
+        self.config["H"]= self.H        
+        # Localizer instance for EKF updates
+        print ('4. Initialise Localizer object')
+        self.Localizer = Localizer(self.config, imudata)
+        
+    # Performs the core navigation computation
+    def baseline(self,W=5, G=5e8, detector='shoe', zv=None):
+        imudata = self.imudata
+        
+        # Initialises the state vector, quaternion, and state covariance using the Localizer
+        x_check,q, P = self.Localizer.init() #initialize state
+        x_hat = x_check 
+        self.x = x_hat
+        
+        if zv is None:
+            ### Compute the trial's zero-velocity detections using the specified detector
+            self.zv = self.Localizer.compute_zv_lrt(W,G, detector=detector) #ZV detection 
+        else:
+            ### Use a pre-computed zero-velocity estimate
+            self.zv = zv
+        print ('11. State prediction using previous')
+        print ('12. Update covariance')
+        print ('13. Corrector step if zero-velocity detected')
+        for k in range(1,x_check.shape[0]):      
+            #predictor
+            if self.config['dt'] is None:
+                dt = self.config['T']
+            else:
+                dt = self.config['dt'][k-1]
+            # State prediction, predict next state based on previous step
+            x_check[k,:], q[k,:],Rot = self.Localizer.nav_eq(x_check[k-1,:], imudata[k,:], q[k-1,:], dt) #update state through motion model
+            # Compute state transition matrix (F) and process noise mapping matrix (G)
+            F,G = self.Localizer.state_update(imudata[k,:],q[k-1,:], dt) 
+            # Update the covariance matrix (P) using the prediction model
+            P[k,:,:] = (F.dot(P[k-1,:,:])).dot(F.T) + (G.dot(self.Q)).dot(G.T)
+            # Symmetrize the covariance matrix to account for numerical approximations
+            P[k,:,:] = (P[k,:,:] + P[k,:,:].T)/2 #make symmetric
+            # Corrector step, zero-velocity is detected, the state is corrected using the Kalman update equations
+            if self.zv[k] == True: 
+                x_hat[k,:], P[k,:,:], q[k,:] = self.Localizer.corrector(x_check[k,:], P[k,:,:], Rot )
+            else:
+                x_hat[k,:] = x_check[k,:]
+            # Store the updated state estimate for this time step
+            self.x[k,:] = x_hat[k,:]  
+        self.x[:,2] = -self.x[:,2] 
+        self.rot = self.x[:,6:9]
+        self.q = q
+        self.P = P
+        # Return the complete state estimates for all time steps
+        print ('14. Return state estimates for all time steps')
+        return self.x
+    
